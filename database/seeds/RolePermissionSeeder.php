9829ab2d4d899e06a8ef7522479570c23e174f3d
Maxim Folko <maxf@leos.co.il>
2022-02-04 21:52:30 +0200

    seed

--- database/seeds/RolePermissionSeeder.php
+++ database/seeds/RolePermissionSeeder.php
@@ -14,28 +14,28 @@ class RolePermissionSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('role_has_permissions')->insert([
-            [
-                'permission_id' => 4,
-                'role_id' => 2,
-            ],
-            [
-                'permission_id' => 5,
-                'role_id' => 2,
-            ],
-            [
-                'permission_id' => 5,
-                'role_id' => 4,
-            ],
-            [
-                'permission_id' => 6,
-                'role_id' => 2,
-            ],
-            [
-                'permission_id' => 6,
-                'role_id' => 3,
-            ],
-        ]);
+//        DB::table('role_has_permissions')->insert([
+//            [
+//                'permission_id' => 4,
+//                'role_id' => 2,
+//            ],
+//            [
+//                'permission_id' => 5,
+//                'role_id' => 2,
+//            ],
+//            [
+//                'permission_id' => 5,
+//                'role_id' => 4,
+//            ],
+//            [
+//                'permission_id' => 6,
+//                'role_id' => 2,
+//            ],
+//            [
+//                'permission_id' => 6,
+//                'role_id' => 3,
+//            ],
+//        ]);
 
 
         DB::table('model_has_roles')->insert([
@@ -44,21 +44,21 @@ class RolePermissionSeeder extends Seeder
                 'model_type' => 'App\\Models\\User',
                 'model_id' => 1,
             ],
-            [
-                'role_id' => 2,
-                'model_type' => 'App\\Models\\User',
-                'model_id' => 4,
-            ],
-            [
-                'role_id' => 3,
-                'model_type' => 'App\\Models\\User',
-                'model_id' => 2,
-            ],
-            [
-                'role_id' => 4,
-                'model_type' => 'App\\Models\\User',
-                'model_id' => 3,
-            ],
+//            [
+//                'role_id' => 2,
+//                'model_type' => 'App\\Models\\User',
+//                'model_id' => 4,
+//            ],
+//            [
+//                'role_id' => 3,
+//                'model_type' => 'App\\Models\\User',
+//                'model_id' => 2,
+//            ],
+//            [
+//                'role_id' => 4,
+//                'model_type' => 'App\\Models\\User',
+//                'model_id' => 3,
+//            ],
         ]);
     }
 }

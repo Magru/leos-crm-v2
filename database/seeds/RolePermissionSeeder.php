3ef9e79eae3dda71a138b349c1eb9c03b9cbbe31
Maxim Folko <maxf@leos.co.il>
2022-02-07 16:24:02 +0200

    seed

--- database/seeds/RolePermissionSeeder.php
+++ database/seeds/RolePermissionSeeder.php
@@ -38,12 +38,12 @@ class RolePermissionSeeder extends Seeder
 //        ]);
 
 
-        DB::table('model_has_roles')->insert([
-            [
-                'role_id' => 1,
-                'model_type' => 'App\\Models\\User',
-                'model_id' => 1,
-            ],
+//        DB::table('model_has_roles')->insert([
+//            [
+//                'role_id' => 1,
+//                'model_type' => 'App\\Models\\User',
+//                'model_id' => 1,
+//            ],
 //            [
 //                'role_id' => 2,
 //                'model_type' => 'App\\Models\\User',
@@ -59,6 +59,6 @@ class RolePermissionSeeder extends Seeder
 //                'model_type' => 'App\\Models\\User',
 //                'model_id' => 3,
 //            ],
-        ]);
+//        ]);
     }
 }
